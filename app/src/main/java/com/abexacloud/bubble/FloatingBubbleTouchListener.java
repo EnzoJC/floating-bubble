e37c8d4662f0824d4cbbe5a662af63c5800bb9ce
Enzo Carrión <ecarrion.abx@gmail.com>
2021-12-30 17:35:21 -0500

    First Commit

---
+++ app/src/main/java/com/abexacloud/bubble/FloatingBubbleTouchListener.java
@@ -0,0 +1,15 @@
+package com.abexacloud.bubble;
+
+public interface FloatingBubbleTouchListener {
+
+    void onDown(float x, float y);
+
+    void onTap(boolean expanded);
+
+    void onRemove();
+
+    void onMove(float x, float y);
+
+    void onUp(float x, float y);
+
+}
